b14e25b98eb6b6ac28f4f6a5edb4ba9771951c4f
Hyunwoo Song <yunuo468@gmail.com>
2025-05-15 16:44:36 +0900

    fix: set redis env

--- src/main/kotlin/config/RedisConfig.kt
+++ src/main/kotlin/config/RedisConfig.kt
@@ -6,13 +6,17 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.listener.RedisMessageListenerContainer
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 
 @Configuration
-class RedisConfig {
+class RedisConfig (
+    private val redisProperties: RedisProperties
+) {
 
     @Bean
-    fun redisConnectionFactory() = LettuceConnectionFactory()
-
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+    }
     @Bean
     fun redisTemplate(): RedisTemplate<String, String> {
         val template = RedisTemplate<String, String>()
